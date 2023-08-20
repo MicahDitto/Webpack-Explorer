e68cf95ade63e9f85c5cf5b9e7a43cf26764ef4e
MicahDitto <35197276+MicahDitto@users.noreply.github.com>
2023-08-20 00:35:41 -0400

    4 files modified, 4 added

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development', // or 'production'
+  entry: {
+    index: './src/index.js',
+    about: './src/about.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      chunks: ['index'],
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/about.html',
+      chunks: ['about'],
+      filename: 'about.html',
+    }),
+  ],
 };
